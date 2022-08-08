2995bcce66db01d99a15c82f47485565af67b692
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-08-07 21:46:03 -0300

    feat(api): Cotações sendo servidas no formato RESTFull
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,16 @@
-from django.http import HttpResponse
-from .data import data
+from rest_framework import serializers, viewsets
+from rest_framework.response import Response
+from .models import Quote
 
 
-def index(request):
-    data()
-    return HttpResponse("O mulher, salva ai no db! #mulherLibera")
+class QuoteSerializer(serializers.HyperlinkedModelSerializer):
+    code = serializers.CharField(source='parent_company.code')
+
+    class Meta:
+        model = Quote
+        fields = ['code', 'value', 'timestamp']
+
+
+class QuoteViewSet(viewsets.ModelViewSet):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
