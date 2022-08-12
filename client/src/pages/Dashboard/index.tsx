e0d3215667b656f3daaa4b4623982222f09f15ea
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-08-12 19:39:19 -0300

    feat(client): Aprimoramento do Dashboard
    Mostrando nome da empresa, código na bolsa e última ação salva no db
    
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,7 +1,51 @@
+import styles from "./styles.module.scss";
+
+import { useEffect, useState } from "react";
+import { Layout } from "../../components/Layout";
+import api from "../../services/api";
+
 export const Dashboard = () => {
+  type Company = {
+    name: string;
+    code: string;
+    last_quote: number;
+  };
+
+  const [companies, setCompanies] = useState([] as Company[]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    updateUi();
+  }, []);
+
+  function updateUi() {
+    setIsLoading(true);
+
+    api
+      .get("/companies/")
+      .then(({ data }) => {
+        setCompanies(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }
   return (
-    <>
-      <p>Olá mundo</p>
-    </>
+    <div className={styles.dashboard}>
+      <Layout hero>
+        <ul className={styles.companies}>
+          {companies.map((company, index) => (
+            <li key={index}>
+              <h3>{company.name}</h3>
+              <p>{company.code}</p>
+              <p>{company.last_quote} BRL</p>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    </div>
   );
 };
