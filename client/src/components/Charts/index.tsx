68f6629e467dac81fba453adf7ab5121be8a95bf
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-08-15 09:17:45 -0300

    feat(client): Gráficos de cotações adicionados
    Agora é possível obter o detalhamento de uma empresa
    
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

---
+++ client/src/components/Charts/index.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { CategoryScale, ScriptableContext } from "chart.js";
+import "chartjs-adapter-date-fns";
+import { ptBR } from "date-fns/locale";
+import api from "../../services/api";
+
+Chart.register(CategoryScale);
+
+type ChartsProps = {
+  companyId: string;
+};
+
+export const Charts = ({ companyId }: ChartsProps) => {
+  type CompanyDetail = {
+    name: string;
+    code: string;
+    last_quote: number;
+    data: { timestamp: Date; value: Number }[];
+  };
+
+  const [company, setCompany] = useState({} as CompanyDetail);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    api
+      .get(`/companies/${companyId}/`)
+      .then(({ data }) => {
+        setCompany(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <Line
+        data={{
+          datasets: [
+            {
+              data: company.data,
+              fill: "start",
+              borderColor: "rgba(148, 187, 233, 1)",
+              borderWidth: 2,
+              backgroundColor: (context: ScriptableContext<"line">) => {
+                const ctx = context.chart.ctx;
+                const gradient = ctx.createLinearGradient(0, 0, 0, 200);
+                gradient.addColorStop(0, "rgba(148, 187, 233, 1)");
+                gradient.addColorStop(1, "rgba(148, 187, 233, 0.2)");
+                return gradient;
+              },
+            },
+          ],
+        }}
+        options={{
+          responsive: true, // Responsividade
+          plugins: {
+            legend: {
+              // Escondendo legenda superior
+              display: false,
+            },
+          },
+          elements: {
+            line: {
+              // Tornando linhas curvadas
+              tension: 0.3,
+            },
+          },
+          scales: {
+            x: {
+              // Definindo que o eixo x é baseado temporal
+              type: "time",
+              time: {
+                unit: "hour",
+              },
+              adapters: {
+                date: {
+                  locale: ptBR,
+                },
+              },
+            },
+          },
+          parsing: {
+            // Definindo x e y
+            xAxisKey: "timestamp",
+            yAxisKey: "value",
+          },
+        }}
+      />
+    </>
+  );
+};
--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import styles from "./styles.module.scss";
-
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "../../components/Layout";
+import { Charts } from "../../components/Charts";
 import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+
   type Company = {
     name: string;
     code: string;
@@ -14,6 +17,7 @@ export const Dashboard = () => {
   const [companies, setCompanies] = useState([] as Company[]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [chart, setChart] = useState("");
 
   useEffect(() => {
     updateUi();
@@ -33,15 +37,33 @@ export const Dashboard = () => {
         setIsLoading(false);
       });
   }
+
+  function handleLineClick(companyId: string) {
+    setChart(companyId);
+    navigate(`#${companyId}`, { replace: true });
+  }
+
   return (
     <div className={styles.dashboard}>
       <Layout hero>
         <ul className={styles.companies}>
           {companies.map((company, index) => (
-            <li key={index}>
-              <h3>{company.name}</h3>
-              <p>{company.code}</p>
-              <p>{company.last_quote} BRL</p>
+            <li
+              key={index}
+              className={chart == company.code ? styles.active : ""}
+              onClick={(e) => handleLineClick(company.code)}
+              id={company.code}
+            >
+              <div className={styles.content}>
+                <h3>{company.name}</h3>
+                <p>{company.code}</p>
+                <p>{company.last_quote} BRL</p>
+              </div>
+              <div className={styles.chart}>
+                {chart == company.code ? (
+                  <Charts companyId={company.code} />
+                ) : null}
+              </div>
             </li>
           ))}
         </ul>
