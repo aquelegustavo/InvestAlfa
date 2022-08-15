ea0e73fb3eeda8bd9647c747faa760169aa1f9ba
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-08-15 12:37:54 -0300

    refactor(api): Definição do 'code' como chave primária da empresa
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- companies/models.py
+++ companies/models.py
@@ -3,16 +3,15 @@ from django.db import models
 
 class Company(models.Model):
     """
-        Empresa
+    Empresa
 
-        Attributes:
-            name: Nome da empresa
-            code: Código da bolsa da empresa
-            last_quote: Última cotação da empresa
+    Attributes:
+        code: Código da bolsa da empresa
+        name: Nome da empresa
+        last_quote: Última cotação da empresa
 
         """
-
+    code = models.CharField(primary_key=True, max_length=5)
     name = models.CharField(max_length=100)
-    code = models.CharField(max_length=5)
     last_quote = models.DecimalField(
         max_digits=5, decimal_places=2, null=True, blank=True, default=None)
