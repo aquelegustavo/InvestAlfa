fbb453e79f0949ae6eb2a7350c2659d042cb198a
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-08-16 03:03:43 -0300

    docs: Documentação das configurações melhorada
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

---
+++ investalfa/__init__.py
@@ -0,0 +1,10 @@
+"""
+InvestAlfa
+
+Sistema para auxiliar um investidor nas suas decisões de compra/venda de ativos
+
+Por Gustavo Simões <gustavosimoesmendonca@gmail.com>
+
+"""
+
+__version__ = "0.1.0"
