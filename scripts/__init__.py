19ee810c0c0b53789dfe5cffbd0a6daf5a98d564
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-08-16 02:50:30 -0300

    docs: Inicio do projeto com Docusaurus para documentação
    Uso da biblioteca pytdoctor para referência de código autogerada
    
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

---
+++ scripts/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+
+def start():
+    os.system("python manage.py runserver")
+
+
+def ref():
+    command = ""
+    cmds = ["pydoctor",
+            "--project-name=InvestAlfa",
+            "--project-version=0.1.0",
+            "--project-url=https://github.com/aquelegustavo/InvestAlfa",
+            "--html-viewsource-base=https://github.com/aquelegustavo/InvestAlfa/tree/main",
+            "--make-html",
+            "--html-output=./docs/static/ref",
+            "--docformat=google",
+            "--intersphinx=https://docs.python.org/3/objects.inv",
+            "./investalfa"]
+
+    for cmd in cmds:
+        command += cmd + " "
+
+    os.system(command)
